f759fc7baa28d7f0909561982a58c8a4d10ae516
eslerkang <eslerkang@gmail.com>
2022-02-12 18:59:41 +0900

    FEAT: Winston Logger 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { utilities, WinstonModule } from 'nest-winston';
 import { AuthModule } from './auth/auth.module';
 import typeORMConfig from './config/typeorm.config';
+import * as winston from 'winston';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { utilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
   const serverConfig = config.get('server');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format:
+            process.env.NODE_ENV === 'production'
+              ? winston.format.simple()
+              : winston.format.combine(
+                  winston.format.timestamp(),
+                  utilities.format.nestLike('Couti', { prettyPrint: true }),
+                ),
+        }),
+      ],
+    }),
+  });
   await app.listen(serverConfig.port);
-  logger.log(`Server Listening at ${serverConfig.port}`);
+  logger.log(`App Listening at localhost:${serverConfig.port}`);
 }
 bootstrap();
