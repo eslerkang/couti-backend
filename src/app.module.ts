f966c79dd198a5337e1e95fa32211db116cd98ec
eslerkang <eslerkang@gmail.com>
2022-02-12 06:49:22 +0000

    FEAT: TypeORM Migration 설정 후 users table migration 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UtilModule } from './util/util.module';
 import { AuthModule } from './auth/auth.module';
-import * as config from 'config';
-
-const dbConfig = config.get('db');
+import typeORMConfig from './config/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: dbConfig.type,
-      host: process.env.RDS_HOST || dbConfig.host,
-      port: process.env.RDS_PORT || dbConfig.port,
-      username: process.env.RDS_USERNAME || dbConfig.username,
-      password: process.env.RDS_PASSWORD || dbConfig.password,
-      database: process.env.RDS_DATABASE || dbConfig.database,
-      entities: dbConfig.entities,
-      synchronize: dbConfig.synchronize,
-    }),
-    UtilModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
   controllers: [],
   providers: [],
 })
--- src/util/time.entity.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export abstract class TimeEntity extends BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
